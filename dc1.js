f684ac012fa9d978dc3d4f74560ca6d8bb3cc713
{test} <000.tgtg.000@gmail.com>
2023-08-09 17:47:08 +0900

    test

--- dc1.js
+++ dc1.js
@@ -1,14 +1,21 @@
-document.getElementById('btn1').addEventListener('click', async () => {
-    // ディレクトリ選択ダイアログを表示して
-    // FileSystemDirectoryHandle オブジェクトを取得
-    const root = await window.showDirectoryPicker();
-   
-    // 開いたディレクトリ内のファイルとディレクトリをコンソールに出力
-    for await (const handle of root.values()) {
-      if (handle.kind === 'file') {
-        console.log(handle.name);
-      } else if (handle.kind === 'directory') {
-        console.log(handle.name + '/');
-      }
+async function readAllFilesInDirectory(directoryHandle) {
+  for await (const entry of directoryHandle.values()) {
+    if (entry.isFile) {
+      let file = await entry.getFile();
+      let content = await file.text();
+      console.log(`File: ${entry.name}\nContent: ${content}\n`);
+    } else if (entry.isDirectory) {
+      let subDirectoryHandle = await entry.getDirectory();
+      await readAllFilesInDirectory(subDirectoryHandle);
     }
+  }
+}
+
+document.getElementById('btn1').addEventListener('click', async () => {
+  try {
+    let initialDirectoryHandle = await window.showDirectoryPicker();
+    await readAllFilesInDirectory(initialDirectoryHandle);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 });
